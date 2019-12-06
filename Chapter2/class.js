945217e8dc56bfa94302ec82f94fdac8516a1be2
Dang Quoc Khanh <khanhdangbmt@gmail.com>
2019-12-07 00:18:10 +0700

    class, extends

---
+++ Chapter2/class.js
@@ -0,0 +1,22 @@
+class Human{
+    constructor(){
+        this.gender = 'male';
+    }
+    printMyGender(){
+        console.log(this.gender);
+    }
+};
+
+class Person extends Human{
+    constructor(){
+        super();
+        this.name = "Khanh";
+    }
+    printMyName(){
+        console.log(this.name);
+    }
+    
+}
+const person = new Person();
+person.printMyName();
+person.printMyGender();
