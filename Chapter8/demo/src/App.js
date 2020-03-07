3d0b9193a52da49d65ccc413ca09e97fb4227eea
Dang Quoc Khanh <khanhdangbmt@gmail.com>
2020-03-07 21:58:07 +0700

    setting up router package

--- Chapter8/demo/src/App.js
+++ Chapter8/demo/src/App.js
@@ -2,19 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
- 
-  render () {
+
+  render() {
     return (
-      <div className="App">
-        <Layout>
-         <BurgerBuilder></BurgerBuilder> 
-        </Layout>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Layout>
+            <BurgerBuilder></BurgerBuilder>
+          </Layout>
+        </div>
+      </BrowserRouter>
     );
   }
-  
+
 }
 
 export default App;
---
+++ chapter9.1/demo/src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import axios from '../../../axios';
+import Post from '../../../components/Post/Post';
+import './Posts.css';
+
+class Posts extends Component {
+    state = {
+        posts: [],
+        error: false
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({
+            selectedPostId: id
+        })
+    }
+
+    componentDidMount() {
+        axios.get('/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPost = posts.map(post => {
+                    return {
+                        ...post,
+                        author: "Khanh"
+                    }
+                })
+                this.setState({
+                    posts: updatedPost
+                })
+                console.log(updatedPost);
+            })
+            .catch(er => {
+                console.log(er);
+                //this.setState({error: true});
+            })
+    }
+
+    render () {
+        let posts = <p style={{ textAlign: "center", color: "red", fontWeight: "bold" }}>Something went wrong!!!</p>
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post
+                    title={post.title}
+                    key={post.id}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)}></Post>
+            });
+        }
+
+        return (
+            <section className="Posts">
+                {posts}
+            </section>
+        );
+    }
+}
+
+export default Posts;
