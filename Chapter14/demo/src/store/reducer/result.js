f29ecd5d3883123b2df8b3c0921a4e7473b21a1d
Dang Quoc Khanh <khanhdangbmt@gmail.com>
2020-03-30 16:53:14 +0700

    install redux, demo redux for project incre, decre value

---
+++ Chapter14/demo/src/store/reducer/result.js
@@ -0,0 +1,26 @@
+import * as actionType from '../action';
+
+const initialState = {
+    results: []
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case actionType.STORE_RESULT: {
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value: action.result})
+            }
+        }
+        case actionType.DELETE_RESULT: {
+            const updatedArray = state.results.filter(result => result.id !== action.resultId);
+            return {
+                ...state,
+                results: updatedArray
+            }
+        }
+    } 
+    return state;
+}
+
+export default reducer;
